f8d554807454e69e82688a6a806ebac2a0c94608
Iona Macbeth <ionalmacbeth@gmail.com>
2018-07-18 12:06:24 +0100

    book constructors

--- src/main/java/models/Book.java
+++ src/main/java/models/Book.java
@@ -1,4 +1,18 @@
 package models;
 
 public class Book {
+
+ private int id;
+ private String title;
+ private String author;
+ private boolean onLoan;
+
+    public Book() {
+    }
+
+    public Book(String title, String author, boolean onLoan) {
+        this.title = title;
+        this.author = author;
+        this.onLoan = onLoan;
+    }
 }
