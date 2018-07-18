e363959231bcacba09b0c3b384a83de4314fcf5f
Iona Macbeth <ionalmacbeth@gmail.com>
2018-07-18 12:26:36 +0100

    borrower added

---
+++ src/main/java/models/Borrower.java
@@ -0,0 +1,42 @@
+package models;
+import models.Book;
+
+import java.util.Set;
+
+    public class Borrower {
+
+        private int id;
+        private String name;
+        private Set<Book> itemsBorrowed;
+
+        public Borrower() {}
+
+        public Borrower(String name) {
+            this.name = name;
+        }
+
+        public int getId() {
+            return id;
+        }
+        public void setId(int id) {
+            this.id = id;
+        }
+
+        public String getName() {
+            return name;
+        }
+        public void setName(String name) {
+            this.name = name;
+        }
+
+        public Set<Book> getItemsBorrowed() {
+            return itemsBorrowed;
+        }
+        public void setItemsBorrowed(Set<Book> itemsBorrowed) {
+            this.itemsBorrowed = itemsBorrowed;
+        }
+
+
+
+    }
+
